c8dbf1667d274b96a84f46670e1c8154bc373485
StringsOfArrays <pascalmehl@aol.com>
2019-09-15 22:32:29 +0200

    please work now

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import '../styles/index.scss';
-import layoutStyles from './layout.module.scss';
+import layoutStyles from './Layout.module.scss';
 
 const Layout = (props) => {
 
